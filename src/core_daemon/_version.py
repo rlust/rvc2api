bf57fec6f5c5f37959fe0b93672439b5b111fb23
Ryan Holt <ryan@ryanholt.net>
2025-05-09 11:07:42 -0400

    align to format requirements in linters

--- src/core_daemon/_version.py
+++ src/core_daemon/_version.py
@@ -1,5 +1,3 @@
 from pathlib import Path
 
-VERSION = (
-    Path(__file__).resolve().parent.parent.parent / "VERSION"
-).read_text().strip()
+VERSION = (Path(__file__).resolve().parent.parent.parent / "VERSION").read_text().strip()
